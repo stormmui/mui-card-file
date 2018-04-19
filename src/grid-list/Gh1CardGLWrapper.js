528e505605120594df23c298af2bed672a17f498
Michael Angerman <stormasm@gmail.com>
2018-04-18 20:19:07 -0700

    starting to work on fetching data, so need a class structure instead

--- src/grid-list/Gh1CardGLWrapper.js
+++ src/grid-list/Gh1CardGLWrapper.js
@@ -1,19 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 // Both ways work
-import tileData from "./../data/repos/html5-node-diagram.json";
-//import tileData from "./../data/repos/ivy.json";
+//import tileData from "./../data/repos/html5-node-diagram.json";
+import tileData from "./../data/repos/ivy.json";
 //import tileData from "./../data/repos/nodejs-sandboxed-fs.json";
 //import tileData from "./../data/repos/tileData";
 import Gh1CardGL from "./Gh1CardGL";
 
-function Gh1CardGLWrapper(props) {
-
-  return (
-    <div>
-      <Gh1CardGL tileData={tileData}/>
-    </div>
-  );
+class Gh1CardGLWrapper extends React.Component {
+  render() {
+    return (
+      <div>
+        <Gh1CardGL tileData={tileData} />
+      </div>
+    );
+  }
 }
 
 Gh1CardGLWrapper.propTypes = {
