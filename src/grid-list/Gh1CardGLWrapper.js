7c283f2aca0040963e325af352119ef11fb4f636
Michael Angerman <stormasm@gmail.com>
2018-04-19 09:45:47 -0700

    progress

--- src/grid-list/Gh1CardGLWrapper.js
+++ src/grid-list/Gh1CardGLWrapper.js
@@ -1,17 +1,57 @@
 import React from "react";
 import PropTypes from "prop-types";
-// Both ways work
-//import tileData from "./../data/repos/html5-node-diagram.json";
-import tileData from "./../data/repos/ivy.json";
-//import tileData from "./../data/repos/nodejs-sandboxed-fs.json";
-//import tileData from "./../data/repos/tileData";
-import Gh1CardGL from "./Gh1CardGL";
+//import Gh1CardGL from "./Gh1CardGL";
 
 class Gh1CardGLWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {},
+      isLoading: false,
+      error: null
+    };
+  }
+
+  componentDidMount() {
+    //const url = 'https://raw.githubusercontent.com/stormasm/mui-card-file/master/src/data/repos/ivy.json';
+    //const url = 'https://raw.githubusercontent.com/stormasm/mui-card-file/master/src/data/repos/html5-node-diagram.json';
+    const url = 'https://raw.githubusercontent.com/stormasm/mui-card-file/master/src/data/repos/nodejs-sandboxed-fs.json';
+    // const url = "https://hn.algolia.com/api/v1/search?query=redux";
+
+    this.setState({ isLoading: true });
+
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Something went wrong ...");
+        }
+      })
+      .then(data => this.setState({ data, isLoading: false }))
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+
   render() {
+    const hits = this.state.data || [];
+
+    if (this.state.error) {
+      return <p>{this.state.error.message}</p>;
+    }
+
+    if (this.state.isLoading) {
+      return <p>Loading ...</p>;
+    }
+
     return (
+      /*
       <div>
-        <Gh1CardGL tileData={tileData} />
+        <Gh1CardGL tileData={this.state.data} />
+      </div>
+*/
+      <div>
+        {hits.length}
       </div>
     );
   }
